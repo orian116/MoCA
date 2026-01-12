517de168139071c6f29002ab2e888180625a5b87
orian116 <54253604+orian116@users.noreply.github.com>
2026-01-12 13:02:40 -0500

    Add generate_shared_heatmap to module exports

--- src/moca/__init__.py
+++ src/moca/__init__.py
@@ -8,9 +8,11 @@ visualizing feature distributions, and comparing embeddings.
 from .align_to_gold import align_conditions_to_states
 from .plot_hist_overlays import plot_old_new_hist_overlays
 from .umap_alignment_viz import plot_umap_alignment
+from .shared_heatmap import generate_shared_heatmap
 
 __all__ = [
     "align_conditions_to_states",
     "plot_old_new_hist_overlays",
     "plot_umap_alignment",
+    "generate_shared_heatmap",
 ]
